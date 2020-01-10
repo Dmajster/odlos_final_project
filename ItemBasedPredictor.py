e2727493055bde8cd7e92350a6f1768b3d9428d2
dmajster <domen.rostohar98@gmail.com>
2020-01-10 10:04:28 +0100

    still slow

--- ItemBasedPredictor.py
+++ ItemBasedPredictor.py
@@ -1,6 +1,8 @@
 from UserItemData import UserItemData
 from statistics import mean
 from math import sqrt
+import pandas
+from timeit import default_timer as timer
 
 class ItemBasedPredictor:
     def __init__(self, min_values: int = 0, threshold: float = 0):
@@ -9,39 +11,59 @@ class ItemBasedPredictor:
     
     def fit(self, x: UserItemData):
         self.data = x.data
+        
+        movie_ids = self.data['movieID'].unique()
 
-        movies_rated_by_group = self.data.groupby(['movieID'])
-        users_rated_movie_1 = movies_rated_by_group.get_group(1580).set_index('userID')
-        users_rated_movie_2 = movies_rated_by_group.get_group(2716).set_index('userID')
-        users_rated_both = users_rated_movie_1.join(users_rated_movie_2, lsuffix='_1', rsuffix='_2', on='userID', how='inner')
-        print(users_rated_both)
+        print(movie_ids)
+
+        self.data.set_index('userID', inplace=True)
+        self.movie_data_groups = self.data.groupby(['movieID'])
+        self.user_data_groups = self.data.groupby(['userID'])
+
+        for movie_id_1 in movie_ids:
+            for movie_id_2 in movie_ids:
+                if movie_id_1 == movie_id_2:
+                    continue
+                
+                print("ids: \t\t", movie_id_1, movie_id_2)
+
+                start = timer()
+                simmilarity = self.similarity(movie_id_1, movie_id_2)
+                end = timer()
+
+                print("simmilarity \t", simmilarity)
+
+                print("time: \t\t", end - start, "\n")
 
 
     def recommend(self, user_id: int, n: int = 10, rec_seen: bool = True):
         return
 
     def similarity(self, movie_id_1: int, movie_id_2: int):
-        users_rated_movie_1 = self.data['userID'].loc[self.data['movieID'] == movie_id_1].unique()
-        users_rated_movie_2 = self.data['userID'].loc[self.data['movieID'] == movie_id_2].unique()
+        movie_1_group = self.movie_data_groups.get_group(movie_id_1)
+        movie_2_group = self.movie_data_groups.get_group(movie_id_2)
+        movie_both_group = movie_1_group.join(movie_2_group, lsuffix='_1', rsuffix='_2', on='userID', how='inner')        
 
-        users_rated_both = self.data['userID'].loc[
-            self.data['userID'].isin(users_rated_movie_1) &
-            self.data['userID'].isin(users_rated_movie_2)
-        ].unique()
+        if  len(movie_both_group) < self.min_values:
+            return 0
 
         sum_top = 0
         sum_bottom_left = 0
         sum_bottom_right = 0
 
-        for user_id in users_rated_both:
-            user_ratings = self.data.loc[self.data['userID'] == user_id]
-            user_average_rating = mean(user_ratings['rating'])
-            user_rating_movie_1 = user_ratings.loc[self.data['movieID'] == movie_id_1, ['rating']].values[0][0]
-            user_rating_movie_2 = user_ratings.loc[self.data['movieID'] == movie_id_2, ['rating']].values[0][0]
+        for user_id, user_rating_data in movie_both_group.iterrows():
+            user_average_rating = mean(self.user_data_groups.get_group(user_id)['rating'])
+            user_rating_movie_1 = user_rating_data['rating_1']
+            user_rating_movie_2 = user_rating_data['rating_2']
 
             sum_top += (user_rating_movie_1 - user_average_rating) * (user_rating_movie_2 - user_average_rating)
             sum_bottom_left += pow(user_rating_movie_1 - user_average_rating, 2)
             sum_bottom_right += pow(user_rating_movie_2 - user_average_rating, 2)
 
-        return sum_top / ( sqrt(sum_bottom_left) * sqrt(sum_bottom_right) )
+        simmilarity = sum_top / ( sqrt(sum_bottom_left) * sqrt(sum_bottom_right) )
+
+        if simmilarity < self.threshold:
+            return 0
+
+        return simmilarity
             
