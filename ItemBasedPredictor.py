077214ef5e99a2851fe6568653a5dbf10a20249e
dmajster <domen.rostohar98@gmail.com>
2020-01-10 12:22:02 +0100

    big fast

--- ItemBasedPredictor.py
+++ ItemBasedPredictor.py
@@ -1,69 +1,58 @@
 from UserItemData import UserItemData
 from statistics import mean
 from math import sqrt
+
 import pandas
+
 from timeit import default_timer as timer
+
 
 class ItemBasedPredictor:
     def __init__(self, min_values: int = 0, threshold: float = 0):
         self.min_values = min_values
         self.threshold = threshold
-    
+
     def fit(self, x: UserItemData):
         self.data = x.data
-        
-        movie_ids = self.data['movieID'].unique()
 
-        print(movie_ids)
+        movie_ids = self.data['movieID'].unique()
 
         self.data.set_index('userID', inplace=True)
         self.movie_data_groups = self.data.groupby(['movieID'])
-        self.user_data_groups = self.data.groupby(['userID'])
+        self.user_rating_averages = self.data.groupby(['userID'])['rating'].mean()
 
-        for movie_id_1 in movie_ids:
-            for movie_id_2 in movie_ids:
-                if movie_id_1 == movie_id_2:
-                    continue
-                
-                print("ids: \t\t", movie_id_1, movie_id_2)
+        self.similarities = pandas.DataFrame({ 
+            movie_id_1: {
+                movie_id_2: self.similarity(movie_id_1, movie_id_2)
+                for movie_id_2 in movie_ids }
+            for movie_id_1 in movie_ids 
+        })
 
-                start = timer()
-                simmilarity = self.similarity(movie_id_1, movie_id_2)
-                end = timer()
+    def predict(self, user_id: int, n: int = 10, rec_seen: bool = True):
+        
+        test_id = 296
+        
+        test_something = self.similarities.loc[(self.similarities.index == test_id)].to_dict()
 
-                print("simmilarity \t", simmilarity)
+        print(test_something)
 
-                print("time: \t\t", end - start, "\n")
-
-
-    def recommend(self, user_id: int, n: int = 10, rec_seen: bool = True):
         return
 
     def similarity(self, movie_id_1: int, movie_id_2: int):
         movie_1_group = self.movie_data_groups.get_group(movie_id_1)
         movie_2_group = self.movie_data_groups.get_group(movie_id_2)
-        movie_both_group = movie_1_group.join(movie_2_group, lsuffix='_1', rsuffix='_2', on='userID', how='inner')        
+        movie_both_group = movie_1_group.join(movie_2_group, lsuffix='_1', rsuffix='_2', on='userID', how='inner')
 
-        if  len(movie_both_group) < self.min_values:
+        if len(movie_both_group) < self.min_values:
             return 0
 
-        sum_top = 0
-        sum_bottom_left = 0
-        sum_bottom_right = 0
-
-        for user_id, user_rating_data in movie_both_group.iterrows():
-            user_average_rating = mean(self.user_data_groups.get_group(user_id)['rating'])
-            user_rating_movie_1 = user_rating_data['rating_1']
-            user_rating_movie_2 = user_rating_data['rating_2']
-
-            sum_top += (user_rating_movie_1 - user_average_rating) * (user_rating_movie_2 - user_average_rating)
-            sum_bottom_left += pow(user_rating_movie_1 - user_average_rating, 2)
-            sum_bottom_right += pow(user_rating_movie_2 - user_average_rating, 2)
-
-        simmilarity = sum_top / ( sqrt(sum_bottom_left) * sqrt(sum_bottom_right) )
+        movie_both_group = movie_both_group.join(self.user_rating_averages, how='inner')
+        sum_top = sum((movie_both_group['rating_1'] - movie_both_group['rating']) * (movie_both_group['rating_2'] - movie_both_group['rating']))
+        sum_bottom_left = sum(pow(movie_both_group['rating_1'] - movie_both_group['rating'], 2))
+        sum_bottom_right = sum(pow(movie_both_group['rating_2'] - movie_both_group['rating'], 2))
+        simmilarity = sum_top / (sqrt(sum_bottom_left) * sqrt(sum_bottom_right))
 
         if simmilarity < self.threshold:
             return 0
 
         return simmilarity
-            
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from UserItemData import UserItemData 
 from MovieData import MovieData
-from RandomPredictor import RandomPredictor
-from AveragePredictor import AveragePredictor
-from ViewsPredictor import ViewsPredictor
+
+md = MovieData('data/movies.dat')
+uim = UserItemData('data/user_ratedmovies.dat', min_ratings=1300)
+
 from ItemBasedPredictor import ItemBasedPredictor
 from Recommender import Recommender
 
-md = MovieData('data/movies.dat')
-uim = UserItemData('data/user_ratedmovies.dat', min_ratings=1000)
 rp = ItemBasedPredictor()
 rec = Recommender(rp)
 rec.fit(uim)
 
-print("Podobnost med filmoma 'Men in black'(1580) in 'Ghostbusters'(2716): ", rp.similarity(1580, 2716))
-print("Podobnost med filmoma 'Men in black'(1580) in 'Schindler's List'(527): ", rp.similarity(1580, 527))
-print("Podobnost med filmoma 'Men in black'(1580) in 'Independence day'(780): ", rp.similarity(1580, 780))
+print("Predictions for 78: ")
+print(rec.predictor.predict(78))
+
+rec_items = rec.recommend(78, n=15, rec_seen=False)
+
+for idmovie, val in rec_items:
+    print("Film: {}, ocena: {}".format(md.get_title(idmovie), val))
